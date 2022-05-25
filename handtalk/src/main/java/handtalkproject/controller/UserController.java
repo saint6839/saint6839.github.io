68a950b8bc260a3c96e1e9fa3e2cc3223cdc3d47
saint6839 <saint6839@gmail.com>
2022-05-25 23:20:45 +0900

    feat: 이메일 전송하는 테스트 메서드 구현

--- handtalk/src/main/java/handtalkproject/controller/UserController.java
+++ handtalk/src/main/java/handtalkproject/controller/UserController.java
@@ -2,11 +2,15 @@ package handtalkproject.controller;
 
 import handtalkproject.domain.dto.UserSignUpDto;
 import handtalkproject.domain.entity.User;
+import handtalkproject.service.EmailService;
 import handtalkproject.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/users")
@@ -19,9 +23,18 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/")
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("")
     public User create(UserSignUpDto userSignUpDto) {
         User save = userService.save(userSignUpDto.toEntity());
         return save;
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public void test() {
+        emailService.sendSimpleMessage("hyeonji0718@gmail.com", "맨지야", "바보");
     }
 }
