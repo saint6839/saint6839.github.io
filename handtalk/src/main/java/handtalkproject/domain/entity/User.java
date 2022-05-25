324a8b41981aa6f1bb0f0dceda231855e30e2c46
saint6839 <saint6839@gmail.com>
2022-05-25 16:37:31 +0900

    feat: 사용자 엔티티 구현(임시)

---
+++ handtalk/src/main/java/handtalkproject/domain/entity/User.java
@@ -0,0 +1,28 @@
+package handtalkproject.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
