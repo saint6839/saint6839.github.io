046b8896fd3212ae983d2e5700edfd210c7187b2
saint6839 <saint6839@gmail.com>
2022-05-25 23:20:07 +0900

    feat: 이메일 발송 Config 클래스 구현

---
+++ handtalk/src/main/java/handtalkproject/config/JavaMailSenderConfig.java
@@ -0,0 +1,29 @@
+package handtalkproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class JavaMailSenderConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("saint6839@gmail.com");
+        mailSender.setPassword("delta9454@");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
