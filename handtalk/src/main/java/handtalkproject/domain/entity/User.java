a2045996da396b9d35b1de9148a0a22d83b5320b
saint6839 <saint6839@gmail.com>
2022-06-27 22:40:32 +0900

    feat: Lombok Builder 적용

--- handtalk/src/main/java/handtalkproject/domain/entity/User.java
+++ handtalk/src/main/java/handtalkproject/domain/entity/User.java
@@ -1,30 +1,33 @@
 package handtalkproject.domain.entity;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 
 import javax.persistence.*;
 
 @Entity
-@NoArgsConstructor
 @Getter
-@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-
-    private String userId;
-    private String password;
-    private String name;
     private String email;
+    private String password;
+    private String nickname;
+    private String profile;
     private boolean isEmailAuthorized;
 
-    public User(String userId, String password, String name, String email, boolean isEmailAuthorized) {
-        this.userId = userId;
-        this.password = password;
-        this.name = name;
+    @Builder
+    public User(String email, String password, String nickname, String profile, boolean isEmailAuthorized) {
         this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.profile = profile;
         this.isEmailAuthorized = isEmailAuthorized;
+    }
+
+    public User() {
+
     }
 }
