ce292720a1209ed545fa744810a492e8b5a9c7df
saint6839 <saint6839@gmail.com>
2022-06-27 22:40:07 +0900

    feat: 이메일 존재 여부 검증 메서드 선언

--- handtalk/src/main/java/handtalkproject/repository/UserRepository.java
+++ handtalk/src/main/java/handtalkproject/repository/UserRepository.java
@@ -4,4 +4,5 @@ import handtalkproject.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
